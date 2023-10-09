29df9cc9b6f9bb425b2b5006dcfbd3a8cda028a7
joostwmd <88150245+joostwmd@users.noreply.github.com>
2023-10-09 17:55:40 +0200

    feat: new data processor

--- dataProcessor.js
+++ dataProcessor.js
@@ -1,42 +1,99 @@
 // Function to process and export the data
 function processData(csvFilePath) {
-    return new Promise((resolve, reject) => {
-        fetch(csvFilePath)
-        .then(response => response.text())
-        .then(data => {
-          let modifiedData = data.split('\n').slice(1).join('\n');
-          Papa.parse(modifiedData, {
-            header: true,
-            complete: (result) => {
-              const data = result.data;
-    
-              const columnsToAccess = ['Timestamp', 'EEG.AF3', 'EEG.T7', 'EEG.Pz', 'EEG.T8', 'EEG.AF4'];
-              let firstTimestamp = null;
-    
-              const valuesFromColumns = data.map((row) => {
-                const rowData = {};
-                columnsToAccess.forEach((column) => {
+  return new Promise((resolve, reject) => {
+    fetch(csvFilePath)
+      .then(response => response.text())
+      .then(data => {
+        let modifiedData = data.split('\n').slice(1).join('\n');
+        Papa.parse(modifiedData, {
+          header: true,
+          complete: (result) => {
+            const data = result.data;
+            const columns = values = [
+              "Timestamp",
+              "POW.AF3.Alpha",
+              "POW.AF3.BetaH",
+              "POW.AF3.BetaL",
+              "POW.AF3.Gamma",
+              "POW.AF3.Theta",
+              "POW.AF4.Alpha",
+              "POW.AF4.BetaH",
+              "POW.AF4.BetaL",
+              "POW.AF4.Gamma",
+              "POW.AF4.Theta",
+              "POW.Pz.Alpha",
+              "POW.Pz.BetaH",
+              "POW.Pz.BetaL",
+              "POW.Pz.Gamma",
+              "POW.Pz.Theta",
+              "POW.T7.Alpha",
+              "POW.T7.BetaH",
+              "POW.T7.BetaL",
+              "POW.T7.Gamma",
+              "POW.T7.Theta",
+              "POW.T8.Alpha",
+              "POW.T8.BetaH",
+              "POW.T8.BetaL",
+              "POW.T8.Gamma",
+              "POW.T8.Theta"
+            ]
+
+
+            let firstTimestamp = null;
+
+            const valuesFromColumns = data.map((row) => {
+              const rowData = {};
+
+              if (row["POW.AF3.Alpha"] !== '') {
+                console.log(row["POW.AF3.Alpha"] !== '')
+                columns.forEach((column) => {
                   if (column === 'Timestamp') {
-                    // Convert the Unix timestamp to time interval (in seconds) relative to the first row as a floating-point number
                     if (firstTimestamp === null) {
                       firstTimestamp = parseFloat(row[column]);
                     }
                     rowData[column] = (parseFloat(row[column]) - firstTimestamp).toFixed(3);
                   } else {
+
                     rowData[column] = row[column];
                   }
                 });
-                return rowData;
-              });
-    
-              resolve(valuesFromColumns)
-            },
-          });
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          reject(error)
+              }
+              return rowData;
+            });
+
+            const filteredData = valuesFromColumns.filter((row) => {
+              return Object.keys(row).length !== 0
+            })
+
+            console.log('filtered', filteredData)
+
+
+            //average
+            function accumulateRow(row, freqType) {
+              let sum = 0
+              const rowAsArray = Object.entries(row)
+              rowAsArray.forEach(item => {
+                if (item[0].includes(freqType) && item[1]) {
+                  console.log(item)
+                  sum += Number(item[1])
+                }
+              })
+              return sum.toString()
+            }
+
+
+            const accValueData = filteredData.map((row) => {
+              return (
+                { timestamp: row.Timestamp, alpha: accumulateRow(row, 'Alpha'), betaL: accumulateRow(row, 'BetaL'), betaH: accumulateRow(row, 'BetaH'), gamma: accumulateRow(row, 'Gamma'), theta: accumulateRow(row, 'Theta') }
+              )
+            })
+            resolve(accValueData)
+          },
         });
-    })
-  }
-  
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        reject(error)
+      });
+  })
+}
